902ca6a94afc31c886d74d8949a314374bc16a09
roshankareer <rosha320neo@gmail.com>
2024-11-10 11:59:18 -0500

    yay, I got somthing to work (In unamed)

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from manim import *
 
-class HelloWorld(Scene):
+class FirstExample(Scene):
     def construct(self):
-        circ = Circle()
         sq = Square()
-        self.add(circ) 
-        self.add(sq)
+
+        def square_update(s):
+            s.shift(0.2*RIGHT)
+
+        conter = 0 
+
+        while conter<5:
+            conter +=1
+            sq = Square()
+            self.play(Create(sq))
+            sq.add_updater(square_update)
+            self.play(sq.animate())
+            if sq.get_x() >= 7.1:
+                self.remove(sq)
+                
+            
